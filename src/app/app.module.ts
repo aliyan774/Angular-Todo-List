0028d3a0d8f90cd543bb7d03a01908366068350e
aliyan774 <ahmed.aliyan774@gmail.com>
2025-07-23 13:38:43 +0500

    learn about the components services and dependency injection and angular cli basics modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './My-Components/todos/todos.component';
+import { TodoHeaderComponent } from './My-Components/todo-header/todo-header.component';
+import { TodoFooterComponent } from './My-Components/todo-footer/todo-footer.component';
+import { MainComponent } from './My-Components/main/main.component';
+import { TodoListComponent } from "./My-Components/todo-list/todo-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoHeaderComponent,
+    TodoFooterComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    TodoListComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
